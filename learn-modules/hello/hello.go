de1881c77c9b23d26127c9dae126a51d4794ffd6
Joby Francis <joby.francis@silverstripe.com>
2024-10-04 17:38:56 +1300

    modules log math/rand errors

--- learn-modules/hello/hello.go
+++ learn-modules/hello/hello.go
@@ -8,10 +8,13 @@ import (
 )
 
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
 	log.SetPrefix("greetings:")
 	log.SetFlags(0)
 
-	message1, err1 := greetings.Hello("John")
+	/*message1, err1 := greetings.Hello("John")
 	if err1 != nil {
 		log.Fatal(err1)
 	}
@@ -21,5 +24,19 @@ func main() {
 	if err2 != nil {
 		log.Fatal(err2)
 	}
-	fmt.Println(message2)
+	fmt.Println(message2)*/
+
+	// A slice of names.
+	names := []string{"Gladys", "John", "Mary"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	for _, message := range messages {
+		fmt.Println(message)
+	}
 }
