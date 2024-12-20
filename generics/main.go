5bfd522da65cc4102b0b12eca1f798e10dcea879
Joby Francis <joby.francis@silverstripe.com>
2024-12-20 15:14:08 +1300

    generics

---
+++ generics/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	ints := map[string]int64{
+		"first":  34,
+		"second": 12,
+	}
+
+	floats := map[string]float64{
+		"first":  35.98,
+		"second": 26.99,
+	}
+
+	fmt.Printf("Non-generic sums: %v and %v\n", SumInts(ints), SumFloats(floats))
+	fmt.Printf("Generic sums: %v and %v\n", SumIntsorFloats[string, int64](ints), SumIntsorFloats[string, float64](floats))
+	fmt.Printf("Generic sums, type parameters inferred: %v and %v\n", SumIntsorFloats(ints), SumIntsorFloats(floats))
+}
+
+func SumInts(m map[string]int64) int64 {
+	var s int64
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func SumFloats(m map[string]float64) float64 {
+	var s float64
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func SumIntsorFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
